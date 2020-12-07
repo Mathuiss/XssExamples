86dda19b24c51b50a3cbb5c61bebbc0d8d955baf
Jan Baan <jwbaan98@gmail.com>
2020-12-07 11:58:52 +0100

    fixed Stored + Reflected

---
+++ XssFixed/Models/PostModel.cs
@@ -0,0 +1,8 @@
+namespace XssExamples.Models
+{
+    public class PostModel
+    {
+        public string PostName { get; set; }
+        public string PostContent { get; set; }
+    }
+}
--- XssFixed/Pages/Error.cshtml.cs
+++ XssFixed/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace XssFixed.Pages
+namespace XssExamples.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
